508f989fff59cea1ed98be3ce10789bdf95f7cd6
Umair Mirza <umair_hp@hotmail.com>
2021-01-22 01:16:13 +0500

    Prepare for Deployment

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import {notFound, errorHandler} from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json()); //it will allow us to access json data within the body
+
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
+
+const __dirname = path.resolve();
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...!');
+    })
+}
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 7000;
+
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
